c40deb52f88b56938ef16f82c1ca6707e3968c35
yanzi418 <yanzi418@users.noreply.github.com>
2018-09-30 14:41:36 +0800

    Update mqtt_app.c

--- Src/mqtt_api/mqtt_app.c
+++ Src/mqtt_api/mqtt_app.c
@@ -422,7 +422,7 @@ void LanReadySendMsg( void ){
 void MqttCycle( void const * argument )
 {
 	enum msgTypes mqttmsgType ;
-	char *host = "39.108.231.83";
+	char *host = "192.168.0.1";
 	int port = 1885;
 	
     ListInitiate( &MQTT_QUEUE_LIST );
